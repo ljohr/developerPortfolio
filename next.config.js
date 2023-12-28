a2d10fc3ddd52c36cf7180fc6df79cf8565a7e4a
Lilly Ohr <lillyohr@gmail.com>
2023-12-28 12:35:34 -0500

    Added Contentlayer setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true };
+
+module.exports = withContentlayer(nextConfig);
